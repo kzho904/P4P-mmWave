4e2ed1dfc57b60a3cfafadf5bf573d31623c7568
Katie Zhou <katiezh13533@gmail.com>
2024-08-14 15:43:29 +1200

    new data split

--- MMWave_Radar_Human_Tracking_and_Fall_detection/library/human_tracking.py
+++ MMWave_Radar_Human_Tracking_and_Fall_detection/library/human_tracking.py
@@ -86,7 +86,7 @@ class HumanTracking(DataProcessor):
                 
                 point_taken_poss_matrix[c, p] = self.TRK_people_list[p].check_clus_possibility(obj_cp_total[c], obj_size_total[c])
         
-        dir = "2_class_data/walking/point_taken_poss_matrix" + str(self.currentSave) + ".pkl"
+        dir = "3_class_data/speedwalking/point_taken_poss_matrix" + str(self.currentSave) + ".pkl"
         # keep finding the global maximum value of the possibility matrix until no values above 0
         while point_taken_poss_matrix.size > 0 and np.max(point_taken_poss_matrix) > 0:
             
@@ -96,8 +96,8 @@ class HumanTracking(DataProcessor):
             # print(poss_clus_list[c])
            
             if self.window == 10 and self.currentSave != 100:
-                # with open(dir, 'wb') as file:
-                #     pickle.dump(self.totalArray, file)
+                with open(dir, 'wb') as file:
+                    pickle.dump(self.totalArray, file)
                 self.window = 0
                 self.currentSave += 1
                 self.totalArray = []
